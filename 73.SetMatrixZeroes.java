543cbe9b56a11c12bdbb10afcf14e8f34f9f415b
Tingjun Zhang <tz976@nyu.edu>
2018-10-25 22:11:50 -0700

    73. Set Matrix Zeroes

--- 73.SetMatrixZeroes.java
+++ 73.SetMatrixZeroes.java
@@ -2,20 +2,36 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rLength = matrix.length;
         int cLength = matrix[0].length;
-        for (int i = 1; i<rLength; i++) {
-            for (int j = 1; j < cLength; j++) {
+        boolean c0 = false;
+        boolean r0 = false;
+        for (int i = 0; i<rLength; i++) {
+            for (int j = 0; j < cLength; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = matrix[0][j] = 0;
+                    if (i==0) r0 = true;
+                    if (j==0) c0 = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i<rLength; i++) {
-            for (int j = 0; j < cLength; j++) {
+        for (int i = 1; i<rLength; i++) {
+            for (int j = 1; j < cLength; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
+        
+        if (r0) {
+            for (int j = 0; j<cLength; j++) {
+                matrix[0][j] = 0;
+            }
+        } 
+        if (c0) {
+            for (int i = 0; i<rLength; i++) {
+                matrix[i][0] = 0;
+            }
+        } 
     }
 }
