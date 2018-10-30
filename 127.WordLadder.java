6686c7f2386392bf92cb7c5e7289d35e44839f2d
Tingjun Zhang <tz976@nyu.edu>
2018-10-29 22:29:51 -0700

    Word Ladder

---
+++ 127.WordLadder.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> reached = new HashSet<String>();
+        reached.add(beginWord);
+        int distance = 1;
+        while(!reached.contains(endWord)) {
+            Set<String> toAdd = new HashSet<String>();
+            for (String each: reached) {
+                for (int i = 0; i < each.length(); i++) {
+                    char[] chars = each.toCharArray();
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        chars[i] = ch;
+                        String word = new String(chars);
+                        if (wordList.contains(word)) {
+                            toAdd.add(word);
+                            wordList.remove(word);
+                        }
+                    }
+                }
+            }
+            distance++;
+            if (toAdd.size() == 0) return 0;
+            reached = toAdd;
+        }
+        return distance;
+    }
+}
